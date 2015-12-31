8bda6c0a90eee1cb54e8823436607fa6bbb73318
anarkia1976 <stefano.villa1976@gmail.com>
2015-12-31 16:24:08 +0100

    cpufreq: hardlimit: set the correct min/max cpu frequencies for Angler

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -33,10 +33,10 @@
 static unsigned int min_freq_hardlimit[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 static unsigned int max_freq_hardlimit[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-#define CONFIG_MSM_CPU_FREQ_MIN_GROUP1 384000
-#define CONFIG_MSM_CPU_FREQ_MAX_GROUP1 1555200
-#define CONFIG_MSM_CPU_FREQ_MIN_GROUP2 384000
-#define CONFIG_MSM_CPU_FREQ_MAX_GROUP2 1766400
+#define CONFIG_MSM_CPU_FREQ_MIN_GROUP1 302400
+#define CONFIG_MSM_CPU_FREQ_MAX_GROUP1 1708800
+#define CONFIG_MSM_CPU_FREQ_MIN_GROUP2 302400
+#define CONFIG_MSM_CPU_FREQ_MAX_GROUP2 2054400
 
 /**
  * The "cpufreq driver" - the arch- or hardware-dependent low
