0f045a81d3c9b12d4ade2dadd525f233d38618c4
Mark Salyzyn <salyzyn@google.com>
2016-08-26 10:16:28 -0700

    pstore: drop pmsg bounce buffer
    Removing a bounce buffer copy operation in the path is always
    better, but we also gain on performance by not requesting a vmalloc
    on every write as this can cause precious RT tasks to stall while
    memory is being arranged for. We did this with a write_buf handler
    signature change with an enum pstore_from_id argument as part of the
    buffer specification passed into pstore ram core.
    
    Pedantically added error propagation support to the write path in the
    ram core, despite how impossible it is for an error could ever occur.
    
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Bug: 31057326
    Change-Id: I4cdee1cd31467aa3e6c605bce2fbd4de5b0f8caa

--- fs/pstore/ram_core.c
+++ fs/pstore/ram_core.c
@@ -15,15 +15,16 @@
 #include <linux/device.h>
 #include <linux/err.h>
 #include <linux/errno.h>
-#include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/io.h>
+#include <linux/kernel.h>
 #include <linux/list.h>
 #include <linux/memblock.h>
+#include <linux/pstore_ram.h>
 #include <linux/rslib.h>
 #include <linux/slab.h>
+#include <linux/uaccess.h>
 #include <linux/vmalloc.h>
-#include <linux/pstore_ram.h>
 #include <asm/page.h>
 
 struct persistent_ram_buffer {
@@ -295,12 +296,20 @@ ssize_t persistent_ram_ecc_string(struct persistent_ram_zone *prz,
 	return ret;
 }
 
-static void notrace persistent_ram_update(struct persistent_ram_zone *prz,
-	const void *s, unsigned int start, unsigned int count)
+static int notrace persistent_ram_update(struct persistent_ram_zone *prz,
+	const void *s, enum pstore_from_id from_user,
+	unsigned int start, unsigned int count)
 {
 	struct persistent_ram_buffer *buffer = prz->buffer;
-	memcpy(buffer->data + start, s, count);
+	int ret = 0;
+
+	if (from_user == PSTORE_FROM_KERNEL)
+		memcpy(buffer->data + start, s, count);
+	else if (unlikely(__copy_from_user(buffer->data + start,
+					   (const void __user *)s, count) != 0))
+		ret = -EFAULT;
 	persistent_ram_update_ecc(prz, start, count);
+	return ret;
 }
 
 void persistent_ram_save_old(struct persistent_ram_zone *prz)
@@ -327,9 +336,9 @@ void persistent_ram_save_old(struct persistent_ram_zone *prz)
 }
 
 int notrace persistent_ram_write(struct persistent_ram_zone *prz,
-	const void *s, unsigned int count)
+	const void *s, unsigned int count, enum pstore_from_id from_user)
 {
-	int rem;
+	int rem, ret = 0;
 	int c = count;
 	size_t start;
 
@@ -344,16 +353,17 @@ int notrace persistent_ram_write(struct persistent_ram_zone *prz,
 
 	rem = prz->buffer_size - start;
 	if (unlikely(rem < c)) {
-		persistent_ram_update(prz, s, start, rem);
+		ret = persistent_ram_update(prz, s, from_user, start, rem);
 		s += rem;
 		c -= rem;
 		start = 0;
 	}
-	persistent_ram_update(prz, s, start, c);
+	if (likely(!ret))
+		ret = persistent_ram_update(prz, s, from_user, start, c);
 
 	persistent_ram_update_header_ecc(prz);
 
-	return count;
+	return unlikely(ret) ? ret : count;
 }
 
 size_t persistent_ram_old_size(struct persistent_ram_zone *prz)
