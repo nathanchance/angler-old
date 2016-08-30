6327e0b35fe312f746300caceae637f0a2f8c3c3
Joe Maples <joe@frap129.org>
2016-08-30 03:05:46 -0400

    AnyKernel: add frandom compatibility

--- anykernel/anykernel.sh
+++ anykernel/anykernel.sh
@@ -222,6 +222,14 @@ dump_boot;
 # init.flash.rc
 insert_line init.angler.rc "init.flash.rc" after "import init.angler.sensorhub.rc" "import init.flash.rc";
 
+# Add frandom compatibility
+backup_file ueventd.rc;
+insert_line ueventd.rc "frandom" after "urandom" "/dev/frandom              0666   root       root\n";
+insert_line ueventd.rc "erandom" after "urandom" "/dev/erandom              0666   root       root\n";
+backup_file file_contexts;
+insert_line file_contexts "frandom" after "urandom" "/dev/frandom		u:object_r:frandom_device:s0\n";
+insert_line file_contexts "erandom" after "urandom" "/dev/erandom		u:object_r:erandom_device:s0\n";
+
 # end ramdisk changes
 
 write_boot;
